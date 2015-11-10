b3f10b81e6a9a51c0c2735301088c98f16b57cd0
Bodily-Brad <bod12002@byui.edu>
2015-11-10 10:13:02 -0700

    stubbed a couple of functions

--- app/src/main/java/edu/byui/cs246/cs246_09_04/MainActivity.java
+++ app/src/main/java/edu/byui/cs246/cs246_09_04/MainActivity.java
@@ -8,8 +8,12 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Local Variable
+    private int count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,5 +52,18 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private int readCountFromPreferences() {
+        return 0;
+    }
+
+    private void writeCountToPreferences(int count) {
+
+    }
+
+    private void updateCountDisplay() {
+        TextView textView = (TextView) this.findViewById(R.id.textCount);
+        textView.setText(Integer.toString(count) );
     }
 }
