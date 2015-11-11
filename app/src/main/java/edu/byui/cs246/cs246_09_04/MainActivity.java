65198aa2b78b9ab68425d1c5a1139067b311cd0d
Bodily-Brad <bod12002@byui.edu>
2015-11-11 10:56:54 -0700

    Moved some strings to the string resource file Added javaDoc comments created DEFAULT_COUNT constant

--- app/src/main/java/edu/byui/cs246/cs246_09_04/MainActivity.java
+++ app/src/main/java/edu/byui/cs246/cs246_09_04/MainActivity.java
@@ -12,6 +12,8 @@ import android.view.MenuItem;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    // CONSTANTS & SETTINGS
+    private static final int DEFAULT_COUNT = 0;
 
     // Local Variable
     private int count = 0;
@@ -61,37 +63,68 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    /**
+     * Advances the local count by 1
+     * @param view View object calling this method (unused)
+     */
     public void AdvanceCount(View view) {
         incrementCount();
         updateCountDisplay();
     }
 
+    /**
+     * Saves the current count
+     * @param view View object calling this method
+     */
     public void SaveCount(View view) {
         if ( writeCountToPreferences(count) ) {
-            Snackbar.make(view, "Count Saved", Snackbar.LENGTH_LONG)
+            Snackbar.make(view, R.string.feedback_pref_save_successful, Snackbar.LENGTH_SHORT)
                     .setAction("Action", null).show();
         }
     }
 
+    /**
+     * Increments the count by 1
+     */
     private void incrementCount() {
         count++;
     }
 
+    /**
+     * Resets the count to 0
+     */
+    private void resetCount() {
+        count = 0;
+    }
+
+    /**
+     * Attempts to retrieve a saved count value from SharedPreferences
+     * @return the value saved in SharedPreferences, or a default value if no value is store in
+     * SharedPreferences
+     */
     private int readCountFromPreferences() {
         SharedPreferences prefs = getPreferences(MODE_PRIVATE);
-        int count = prefs.getInt("count", 10);
+        int count = prefs.getInt(getString(R.string.pref_key_count), DEFAULT_COUNT);
         return count;
     }
 
+    /**
+     * Attempts to store a count value in SharedPreferences
+     * @param count the value to store in SharedPreferences
+     * @return true if successful
+     */
     private boolean writeCountToPreferences(int count) {
         SharedPreferences prefs = getPreferences(MODE_PRIVATE);
         SharedPreferences.Editor editor = prefs.edit();
-        editor.putInt("count", count);
+        editor.putInt(getString(R.string.pref_key_count), count);
         editor.commit();
 
         return true;
     }
 
+    /**
+     * Updates the disply to reflect the current count
+     */
     private void updateCountDisplay() {
         TextView textView = (TextView) this.findViewById(R.id.textCount);
         textView.setText(Integer.toString(count) );
