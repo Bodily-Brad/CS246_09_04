7f061c479f93ed1ab8bd47fbb16f6147748d3c65
Bodily-Brad <bod12002@byui.edu>
2015-11-11 15:12:06 -0700

    Added some debugging info for SQLite Count is getting stored now, we're getting close...

--- app/src/main/java/edu/byui/cs246/cs246_09_04/MainActivity.java
+++ app/src/main/java/edu/byui/cs246/cs246_09_04/MainActivity.java
@@ -1,5 +1,6 @@
 package edu.byui.cs246.cs246_09_04;
 
+import android.content.ContentValues;
 import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -9,10 +10,13 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Pair;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     // CONSTANTS & SETTINGS
@@ -43,6 +47,7 @@ public class MainActivity extends AppCompatActivity {
 
         // Update our Count textView
         updateCountDisplay();
+        debugDB();
     }
 
     @Override
@@ -87,12 +92,12 @@ public class MainActivity extends AppCompatActivity {
                     .setAction("Action", null).show();
         }
 
-        /*
+
         if ( writeCountToSQLite(count) ) {
             Snackbar.make(view, R.string.feedback_sql_save_successful, Snackbar.LENGTH_SHORT)
                     .setAction("Action", null).show();
         }
-        */
+
     }
 
     /**
@@ -196,9 +201,15 @@ public class MainActivity extends AppCompatActivity {
             sql = "INSERT INTO " + openHelper.getTableName() +
                 " ( " + openHelper.getKeyColumnName() + ", " + openHelper.getValueColumnName() + ")" +
                 " VALUES ( " + openHelper.getCountKey() + ", " + Integer.toString(count) + ");";
+
+            ContentValues values = new ContentValues();
+            values.put(openHelper.getKeyColumnName(), openHelper.getCountKey());
+            values.put(openHelper.getValueColumnName(), DEFAULT_COUNT);
+            db.insert(openHelper.getTableName(), null, values);
+        } else {
+            db.execSQL(sql);
         }
 
-        db.execSQL(sql);
         return true;
     }
 
@@ -208,5 +219,40 @@ public class MainActivity extends AppCompatActivity {
     private void updateCountDisplay() {
         TextView textView = (TextView) this.findViewById(R.id.textCount);
         textView.setText(Integer.toString(count) );
+    }
+
+    private void debugDB() {
+
+        // First, use an openhelper
+        // Again, hopefully, getBaseContext will do whatever we need...
+        CountOpenHelper openHelper = new CountOpenHelper(getBaseContext());
+
+        // Great, now get a readable DB
+        SQLiteDatabase db = openHelper.getReadableDatabase();
+
+        // Show databases
+        List<Pair<String, String>> dbList = db.getAttachedDbs();
+
+        System.out.println("getAttachedDbs()");
+        for (Pair<String, String> pair : dbList) {
+            System.out.println(pair.first + " : " + pair.second);
+        }
+
+        Cursor cursor = db.rawQuery("select * from " + openHelper.getTableName(), null);
+        System.out.println("--------------------");
+        System.out.println("How many rows in our table?");
+        System.out.println("--------------------");
+        System.out.println("cursor.getCount : " + cursor.getCount());
+        System.out.println();
+
+        if (cursor.moveToFirst()) {
+            while (cursor.isAfterLast() == false) {
+                System.out.println("key: " + cursor.getString(cursor.getColumnIndex("key")));
+                System.out.println("value: " + cursor.getString(cursor.getColumnIndex("value")));
+                cursor.moveToNext();
+            }
+
+        }
+
     }
 }
